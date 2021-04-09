2eb9c7bbfe1850668e5b973e29282c9569081493
nanandt <rizkyfatih007@gmail.com>
2021-04-10 00:25:45 +0700

    operasi mtk

---
+++ src/OperasiMtk.java
@@ -0,0 +1,30 @@
+public class OperasiMtk {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a - b);
+        System.out.println(a + b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a % b);
+
+        int c = 10;
+        c += 2;
+        System.out.println(c);
+
+        c -= 3;
+        System.out.println(c);
+
+        c *= 3;
+        System.out.println(c);
+
+        int d = 100;
+
+        d++;
+        System.out.println(d);
+
+        d--;
+        System.out.println(d);
+    }
+}
