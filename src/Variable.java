bb880de91ae53f2f30f4192c5de02a7bdc17c009
nanandt <rizkyfatih007@gmail.com>
2021-04-09 23:38:05 +0700

    variable

---
+++ src/Variable.java
@@ -0,0 +1,38 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "badru";
+
+        int age = 20;
+        String address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "aditiyo budi waluyo";
+        System.out.println(name);
+
+        var firstName = "Kiki";
+        var middleName = "duyu lopme";
+        var lastName = "hiyaiyha";
+
+        /*
+        jika menggunakan var mka value nya harus dimasukan
+        dan tidk bisa di inisialisasi setela declarasi menggunakan var
+         ex : var dulur;
+        dulur = "kamigilan";
+         */
+
+        /*
+        jk menggunakan final maka value nya tidak bisa diubah lagi
+        ex : final String app = "belajar java";
+        app = "belajar golang";
+        jika var valuenya masih bisa diubah
+        ex : var app = "jalan-jalan";
+        app = "kemana nich";
+         */
+
+
+    }
+}
