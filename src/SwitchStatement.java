a902417b05772b44686a548eecc1f16c36dee762
nanandt <rizkyfatih007@gmail.com>
2021-04-12 23:28:37 +0700

    ternary operator

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class SwitchStatement {
+    public static void main(String[] args) {
+        var nilai = "A";
+
+        switch (nilai){
+            case "A":
+                System.out.println("Anda lulus dengan baik");
+                break;
+            case "B":
+                System.out.println("Nilai Anda Lumayan Baik");
+                break;
+            case "C":
+                System.out.println("Anda Tidak Lulus");
+                break;
+            default:
+                System.out.println("Anda Lulus Dengan Baik");
+        }
+
+        // switch lamda
+//        switch (nilai){
+//            case "A" -> System.out.println("Anda Lulus Dengan Baik");
+//            default -> System.out.println("kontol");
+//        }
+    }
 }
---
+++ src/TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 60;
+        String ucapan = nilai >= 60 ? "Selamat Anda lulus" : "Silahkan Coba lagi";
+
+        System.out.println(ucapan);
+    }
+}
