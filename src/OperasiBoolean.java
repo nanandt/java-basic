7fa08a1649c13b3550ae23709ee405e75e6a26af
nanandt <rizkyfatih007@gmail.com>
2021-04-12 22:32:53 +0700

    operasi boolean

---
+++ src/OperasiBoolean.java
@@ -0,0 +1,10 @@
+public class OperasiBoolean {
+
+    public static void main(String[] args) {
+        var nilaiAbsen = 60;
+        var nilaiAkhir = 80;
+
+        System.out.println(nilaiAbsen);
+    }
+
+}
