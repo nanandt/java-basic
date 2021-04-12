1fed695b7954e0b5aeef3832a420a4bbcb794cec
nanandt <rizkyfatih007@gmail.com>
2021-04-12 22:39:17 +0700

    expression, statement and block

---
+++ src/Block.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Block {
+    public static void main(String[] args) {
+        // Assigment Statement
+        double aValue = 8708.234;
+        // Increment Statement
+        aValue++;
+        // Method Invocation Statement
+        System.out.println("Hello World!");
+        // Object Creation Statement
+        Date date = new Date();
+    }
+}
--- src/OperasiBoolean.java
+++ src/OperasiBoolean.java
@@ -4,7 +4,11 @@ public class OperasiBoolean {
         var nilaiAbsen = 60;
         var nilaiAkhir = 80;
 
-        System.out.println(nilaiAbsen);
+        var lulusAbsen = nilaiAbsen >= 60;
+        var lulusNilaiAkhir = nilaiAkhir >= 80;
+
+        var lulus = lulusAbsen && lulusNilaiAkhir;
+        System.out.println(lulus);
     }
 
 }
