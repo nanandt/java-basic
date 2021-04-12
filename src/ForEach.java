707119aa85ed9f04e269cb81751f480ab9967afd
nanandt <rizkyfatih007@gmail.com>
2021-04-13 00:04:19 +0700

    for each

---
+++ src/ForEach.java
@@ -0,0 +1,12 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] random = {
+                "Rizky", "Warteg", "Semangka",
+                "Pecel", "Oreg", "Mie"
+        };
+
+        for (var name : random){
+            System.out.println(name);
+        }
+    }
+}
