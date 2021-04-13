9056ba97724ef8a05c6b0b722ae33ce7e527243f
nanandt <rizkyfatih007@gmail.com>
2021-04-13 13:13:24 +0700

    komentar

---
+++ src/Komentar.java
@@ -0,0 +1,20 @@
+public class Komentar {
+
+    public static void main(String[] args) {
+        System.out.println(sum(10,90));
+    }
+    /*
+    comment multiline
+     */
+
+    /**
+     * Method untk menjumlahkan value1 dan value2
+     * @param value1 angka pertama
+     * @param value2 angka kedua
+     * @return hasil penjumlahan value1 dan value2
+     */
+    static int sum(int value1, int value2){
+        // jumlah value 1 dan value 2
+        return value1 + value2;
+    }
+}
